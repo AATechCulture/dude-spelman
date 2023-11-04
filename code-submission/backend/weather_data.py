b3f2039966bf4721dc2dd4fab6ecacc2fe22e360
zaydenSnacks <zaydenchin1@gmail.com>
2023-11-04 15:10:14 -0500

    bonito flakes

--- code-submission/backend/weather_data.py
+++ code-submission/backend/weather_data.py
@@ -1,21 +1,32 @@
 import requests
 
-"""
- STEPS:
- 1. receive a list of data with longitude and latitude and time
- 2. send back weather data
-"""
-
 # EXAMPLE variables
 # longitude = "47.45" [string]
 # latitude = "-122.31" [string]
 # departue_times = "2023-11-04T13:00:00-05:00" [string]
 
-def get_weather_data(longitude, latitude, departue_times):
+def get_weather_data(longitude, latitude, departue_time):
+    """
+    Fetches weather data for a specific location and time.
+
+    This function takes geographical coordinates (longitude and latitude) and a departure time,
+    then makes a request to the National Weather Service API to retrieve the hourly weather forecast.
+
+    Parameters:
+    - longitude (str): The longitude of the location
+    - latitude (str): The latitude of the location
+    - departure_time (str): The ISO 8601 formatted date and time
+
+    Returns:
+    weather data for the specified time period. If no matching time period is found in the forecast data, None is returned.
+    """
 
     weather_data = requests.get(f"https://api.weather.gov/points/{longitude},{latitude}").json()
     seven_day_hourly_forecast = requests.get(weather_data['properties']["forecastHourly"]).json()
 
     for period in seven_day_hourly_forecast['properties']['periods']:
-        if departue_times >= period['startTime'] and departue_times <= period['endTime']:
-            return period
+        if departue_time >= period['startTime'] and departue_time <= period['endTime']:
+            return period 
+    return None
+
+print(get_weather_data("47.45", "-122.31", "2023-11-04T13:00:00-05:00"))
