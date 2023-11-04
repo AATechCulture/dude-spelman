f099cb7a1fde606be07835409b7865ca67264ed4
Kevin Drew <kdrew6@gatech.edu>
2023-11-04 15:12:49 -0500

    bruh.navbar

---
+++ code-submission/frontend/src/components/Navbar/MenuItems.js
@@ -0,0 +1,15 @@
+export const MenuItems = [
+    // Make a list of Navlinks & their properties.
+    {
+      title : "Cancellation Rating",
+      url : "/",
+      cName : "nav-links",
+      icon : "fa-solid fa-house-user"
+    },
+    {
+      title : "Post Cancellation",
+      url : "/post-cancellation",
+      cName : "nav-links",
+      icon : "fa-solid fa-circle-info"
+    },
+  ];
--- code-submission/frontend/src/routes/PostCancellation.js
+++ code-submission/frontend/src/routes/PostCancellation.js
@@ -1,7 +1,9 @@
 import AltFlights from "../components/AltFlights/AltFlights"
+import Navbar from "../components/Navbar/Navbar"
 function PostCancellation() {
   return (
     <>
+    <Navbar />
     <AltFlights />
     
     
