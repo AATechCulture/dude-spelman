73bf0c55f93180168be62bab24c324a81bf1bb51
Jovan Ford <jovanf@Jovans-MacBook-Pro.local>
2023-11-04 18:03:07 -0500

    styling hotels

--- code-submission/frontend/src/routes/PostCancellation.js
+++ code-submission/frontend/src/routes/PostCancellation.js
@@ -1,10 +1,12 @@
 import AltFlights from "../components/AltFlights/AltFlights";
 import Navbar from "../components/Navbar/Navbar";
+import Hotel from "../components/Hotel/Hotel";
 function PostCancellation() {
   return (
     <>
       <Navbar />
       <AltFlights />
+      <Hotel />
     </>
   );
 }
