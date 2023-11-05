b4e74d2a0719c9413c785bc106ce7894b1c7f2e2
tgaylord <tgaylord2024@gmail.com>
2023-11-05 03:30:11 -0600

    Cleaning up Cancellations

--- code-submission/backend/cancellations.py
+++ code-submission/backend/cancellations.py
@@ -1,22 +1,17 @@
 import airportsdata
 import pandas as pd
-import requests
 import weather_data
-import flight_engine
 from datetime import datetime, timedelta
+<<<<<<< Updated upstream
 import dateutil
 import os
+=======
+>>>>>>> Stashed changes
 
 
 airports = airportsdata.load("IATA")
 top25AirCodes = ["ATL","DFW","DEN","ORD","LAX","JFK","LAS","MCO","MIA","CLT","SEA","PHX","EWR","SFO","IAH","BOS","FLL","MSP","LGA","DTW","PHL","SLC","DCA","SAN","BWI"]
-#parameters = {'access_key'='45cf71b762b4a38a7395848c6b6f5147'}
-#api_result = requests.get('https://api.aviationstack.com/v1/flights', parameters)
-#api_response = api_result.json()
 
-# print(airports['SEA'])
-# print(airports['ATL'])
-# atlData = pd.read_csv('code-submission/backend/dataset/ATL_2010_23.csv')
 
 
 def getFlightCancelNum(departDate: str, airportCode: str):
@@ -69,7 +64,6 @@ def getDataTotData(departDate: str, airportCode: str):
     )
     return airlineData[airlineData["Date"] == dateCleanup(departDate)]
 
-#def runningDelayTotal(depart)
 
 def dateCleanup(incomingDate: str):
     """
@@ -81,7 +75,6 @@ def dateCleanup(incomingDate: str):
         Corrected version of date. Ex: "1/5/23"
     """
     dateSplit = incomingDate.split("/")
-    # print(dateSplit)
     if dateSplit[0][0] == "0":
         dateSplit[0] = dateSplit[0][1:]
     if dateSplit[1][0] == "0":
@@ -100,8 +93,6 @@ def generateAirportLongLatDict():
     """
     airportDict = {}
     for airportCode in top25AirCodes:
-        #tempData = round(airports[airportCode]["lat"],2)
-        #print(tempData)
         airportDict[airportCode] = (round(airports[airportCode]["lat"],2),round(airports[airportCode]["lon"],2))
     return airportDict
 
@@ -109,20 +100,12 @@ def getWeatherBetweenDates(start_date:datetime, end_date:datetime,lat:str,long:s
     """
     (TODO: IGNORE, OUT OF DATE VERSION OF INITIAL HISTORICAL_WEATHER_DATA.PY IMPLEMENTATION)
     """
-    #print("HI")
     current_date = start_date
     while current_date.date() <= end_date.date():
-        #yield current_date
-        #formatted_date = current_date.strftime("%m/%d/%Y")
         iso_time = current_date.strftime("%Y-%m-%dT%H:%M:%S%z")
-        #print(iso_time)
         origin_weather = weather_data.get_weather_data(
-            #"47.45", "-122.31", "2023-11-06T13:00:00-05:00"
-            #lat,long, "2023-11-04T13:00:00-05:00"
-            #iso_time
+            lat,long,iso_time
         )
-        #dateutil.parser.parse(current_date).date()
-        #print(origin_weather)
         temp = origin_weather["temperature"]
         precipitation_prob = origin_weather["probabilityOfPrecipitation"]["value"]
         humidity = origin_weather["relativeHumidity"]["value"]
@@ -139,23 +122,10 @@ def parseWeatherData():
     airportDict = generateAirportLongLatDict()
     print(airportDict)
     for airCode in airportDict.keys():
-        #print(datetime(2020,10,1)+timedelta(hours=12))
-        #print(airportDict[airCode][0])
-        #print(airportDict[airCode][1])
         getWeatherBetweenDates(
             datetime(2020,10,1)+timedelta(hours=12),
             datetime(2020,10,5)+timedelta(hours=12),
             airportDict[airCode][0],
             airportDict[airCode][1]
         )
-    """temp = origin_weather["temperature"]
-    precipitation_prob = origin_weather["probabilityOfPrecipitation"]["value"]
-    humidity = origin_weather["relativeHumidity"]["value"]
-    wind_speed = int(origin_weather["windSpeed"][0])
-    forecast = origin_weather["shortForecast"]
-    """
 
-
-#parseWeatherData()
-# print(getDataTotData(dateCleanup("1/02/2020"),"ATL"))
-# print(getDataTotData("1/9/10", "ATL"))
