86ba0413b8a9f2d0eb82500725b454a54b96ce00
JovanFord <jovan.ford14@gmail.com>
2023-11-05 01:08:51 -0500

    fixed merges

--- code-submission/frontend/src/components/CancelRating/CancelRating.js
+++ code-submission/frontend/src/components/CancelRating/CancelRating.js
@@ -2,26 +2,46 @@ import React, { useState, useEffect } from "react";
 import "./CancelRating.css";
 import axios from "axios";
 
-const CancelRating = () => {
-  const [flightNumber, setFlightNumber] = useState("");
-  const [flightDate, setFlightDate] = useState("");
+const CancelRating = ({
+  flightNumber,
+  setFlightNumber,
+  flightDate,
+  setFlightDate,
+}) => {
   const [percentage, setPercentage] = useState(null);
 
-  // useEffect(() => {
-  //   fetch("/predict-cancellation")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setPercentage(percentage);
-  //       console.log(percentage);
-  //     });
-  // });
+  const handleCalculatePercentage = () => {
+    axios
+      .get(`http://localhost:5000/predict-cancellation`, {
+        params: {
+          flight_number: flightNumber,
+          flight_date: flightDate,
+        },
+      })
+      .then((response) => {
+        setPercentage(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
+<<<<<<< HEAD
     <div className="rating-container">
       <h2 className="rating-header">The chance of your flight being cancelled is:</h2>
       <h1 className="rating">16%</h1>
       <p className="rating-explanation">The rating is the result of our flight cancellation algorithm which is influenced by airport weather conditons.</p>
       </div>
+=======
+    <div>
+      <button onClick={handleCalculatePercentage}>
+        Calculate Cancellation Percentage
+      </button>
+      <h4>Cancellation Percentage:</h4>
+      <p>{percentage}%</p>
+    </div>
+>>>>>>> 546710d (moving variables)
   );
 };
 
--- code-submission/frontend/src/routes/PreCancellation.js
+++ code-submission/frontend/src/routes/PreCancellation.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Forecast from "../components/Forecast/Forecast";
 import CancelRating from "../components/CancelRating/CancelRating";
 import IROPS from "../components/IROPS/irops";
 import Navbar from "../components/Navbar/Navbar";
 
 const PreCancellation = () => {
+  const [flightNumber, setFlightNumber] = useState("");
+  const [flightDate, setFlightDate] = useState("");
   return (
     <div>
       <Navbar />
       <Forecast />
-      <CancelRating />
+      <CancelRating
+        flightNumber={flightNumber}
+        setFlightNumber={setFlightNumber}
+        flightDate={flightDate}
+        setFlightDate={setFlightDate}
+      />
       <IROPS />
     </div>
   );
