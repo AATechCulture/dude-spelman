b3b0b2bd5c92e497bc8487955e884fbfd6a4b847
JovanFord <jovan.ford14@gmail.com>
2023-11-04 20:35:08 -0500

    formatting hotel layouts

--- code-submission/frontend/src/components/Forecast/Forecast.js
+++ code-submission/frontend/src/components/Forecast/Forecast.js
@@ -10,20 +10,29 @@ const Forecast = () => {
       <h1 className="title">Pre-Cancellation Metrics</h1>
       <h2 className="sub-title">Departure Airport Forecast</h2>
       <div className="departure-code">
-        <h3 type="text" value={departureCode}>
-          Enter Departure Airport Code:
-        </h3>
-        <input />
+        <h3>Enter Departure Airport Code:</h3>
+        <input
+          type="text"
+          id="departureCode"
+          value={departureCode}
+          onChange={(e) => setDepartureCode(e.target.value)}
+          placeholder="e.g. JFK"
+        />
       </div>
       {/* For forecast info based on airport departure city */}
       <br />
       <h2>Arrival Airport Forecast</h2>
 
       <div className="arrival-code">
-        <h3 type="text" value={arrivalCode}>
-          Enter Arrival Airport Code:
-        </h3>
-        <input />
+        <h3>Enter Arrival Airport Code:</h3>
+        <input
+          className="arrivalCode"
+          type="text"
+          id="arrivalCode"
+          value={arrivalCode}
+          onChange={(e) => setArrivalCode(e.target.value)}
+          placeholder="e.g. LAX"
+        />
       </div>
       {/* For forecast info based on airport arrival city */}
     </div>
