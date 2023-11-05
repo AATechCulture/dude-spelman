803afac7bdcf6c55be036c94b849eb0914a55258
Elijah Truitt <elijahtr423@gmail.com>
2023-11-04 22:21:39 -0500

    salmon roe

--- code-submission/backend/app.py
+++ code-submission/backend/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, Response
+import cancellation_prediction
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def test():
-    return "hi"
+
+@app.route("predict-cancellation", methods=["GET"])
+def predict_cancellation():
+    data = request.get_json()
+    if not data:
+        return Response(status=400)
+
+    flight_number = data["flight_number"]
+    flight_date = data["flight_date"]
+    result = cancellation_prediction.get_cancellation_percentage(
+        flight_number, flight_date
+    )
+
+    return result
+
 
 @app.route("/flight_info")
 def get_flight_info():
