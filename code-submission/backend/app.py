530a66f3deb6f14a99205ecfe07168eeef9fb18a
zaydenSnacks <zaydenchin1@gmail.com>
2023-11-04 23:43:16 -0500

    changed flight_info to get request

--- code-submission/backend/app.py
+++ code-submission/backend/app.py
@@ -20,14 +20,17 @@ def predict_cancellation():
     return result
 
 
-@app.route("/flight_info", methods=['POST'])
+@app.route("/flight_info", methods=['GET'])
 def get_flight_info():
     
-    data = request.json
-    originLocationCode = data.get('param1')
-    destinationLocationCode = data.get('param2')
-    departureDate = data.get('param3')
-    adults = data.get('param4')
+    data = request.get_json()
+    if not data:
+        return Response(status=400)
+    
+    originLocationCode = data["origin_location_code"]
+    destinationLocationCode = data["destination_location_code"]
+    departureDate = data["departure_date"]
+    adults = data["adults"]
     
     return jsonify(flight_info.get_flight_info(originLocationCode, destinationLocationCode, departureDate, adults))
 
