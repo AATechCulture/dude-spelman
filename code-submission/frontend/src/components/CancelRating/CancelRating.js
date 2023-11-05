69b4c8b970237c494b6be45ef9f41a32cb0b12ad
Kevin Drew <kdrew6@gatech.edu>
2023-11-04 23:35:30 -0500

    reactive forecast

--- code-submission/frontend/src/components/CancelRating/CancelRating.js
+++ code-submission/frontend/src/components/CancelRating/CancelRating.js
@@ -9,7 +9,7 @@ const CancelRating = () => {
     <div className="rating-container">
       <h2 className="rating-header">The chance of your flight being cancelled is:</h2>
       <h1 className="rating">16%</h1>
-      <p className="rating-explanation">The rating is the result of our flight cancellation algorithm which is influenced by airport weather condiitons.</p>
+      <p className="rating-explanation">The rating is the result of our flight cancellation algorithm which is influenced by airport weather conditons.</p>
       </div>
   );
 };
--- code-submission/frontend/src/routes/PostCancellation.js
+++ code-submission/frontend/src/routes/PostCancellation.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Hotel from "../components/Hotel/Hotel";
+import AltFlights from "../components/AltFlights/AltFlights";
+import Rideshare from "../components/Rideshare/Rideshare";
 
 const PostCancellation = () => {
   return (
     <div>
       <Navbar />
+      <AltFlights />
       <Hotel />
+      <Rideshare />
     </div>
   );
 };
