8ddc2a6be13f96353f24fe201349285e8ffb52e8
Kevin Drew <kdrew6@gatech.edu>
2023-11-05 03:55:02 -0600

    bruh

--- code-submission/frontend/src/components/AltFlights/AltFlights.js
+++ code-submission/frontend/src/components/AltFlights/AltFlights.js
@@ -1,20 +1,53 @@
 import "./AltFlights.css";
-function AltFlights() {
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+const AltFlights = () => {
+
+  const [flightData, setFlightData] = useState(null);
+
+  useEffect(() => {
+    const fetchFlightInfo = async () => {
+      try {
+        const response = await axios.post('http://your-server-endpoint/get-flight-info', {
+          originLocationCode: 'MAD',
+          destinationLocationCode: 'BOS',
+          departureDate: '2023-11-06',
+          adults: '1'
+        });
+        setFlightData(response.data);
+      } catch (error) {
+        console.error('Error fetching flight info:', error);
+      }
+    };
+
+    fetchFlightInfo();
+  }, []);
+
+
   return (
-    <>
-    <div className="alt-flights-container">
-        <h1>Alternative Flights available</h1>
-        <p className="alt-flights-text">
-            The flights displayed below are options less at risk for cancellation.
-        </p>
- 
-
-   
-      
+    <div>
+      <h1>Flight Information</h1>
+      {flightData ? (
+        <div>
+          <h2>Flight Offers</h2>
+          <ul>
+            {flightData[0].data.map((offer, index) => (
+              <li key={index}>
+                <h3>Offer {index + 1}</h3>
+                <p>Airline: {offer.offerItems[0].services[0].segments[0].flightSegment.carrierCode}</p>
+                <p>Departure: {offer.offerItems[0].services[0].segments[0].flightSegment.departure.iataCode}</p>
+                <p>Arrival: {offer.offerItems[0].services[0].segments[0].flightSegment.arrival.iataCode}</p>
+                {/* Add more details as needed */}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Loading flight information...</p>
+      )}
     </div>
-
-    </>
+    
   );
-}
+};
 
 export default AltFlights;
